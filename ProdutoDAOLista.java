2aaf685d45dc02876958028ab549639473608b50
Brayan_Alves <brayanalvesdossantos67@gmail.com>
2026-03-17 09:49:30 -0300

    tarefa v2

--- ProdutoDAOLista.java
+++ ProdutoDAOLista.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProdutoDAOLista implements ProdutoDAO {
+public class ProdutoDAOLista implements ProdutoDAO{
     private List<Produto> produtos = new ArrayList<>();
 
     @Override
